8181eafa5cf08a3580f03b29966405b8a45b5474
Kavin Abregana <abreganakavin@gmail.com>
2023-07-11 00:14:41 +0800

    learning about prototypes

--- script.js
+++ script.js
@@ -23,3 +23,46 @@ const matilda = new Person('Matilda', 2017);
 
 console.log(jonas instanceof Person);
 // result: true
+Person.prototype.calcAge = function () {
+  console.log(new Date().getFullYear() - this.birthYear);
+};
+
+jonas.calcAge();
+// result: 31
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+// result: true
+console.log(Person.prototype.isPrototypeOf(Person));
+// result: false
+
+// creates a new property in the Person Object
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+// result: Homo Sapiens Homo Sapiens
+
+console.log(jonas.hasOwnProperty('firstname'));
+// result: true
+console.log(jonas.hasOwnProperty('species'));
+// result: false
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [0, 3, 2, 5, 2, 7, 9, 6, 1, 2, 3, 4, 5, 6];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.log(h1);
+console.dir(x => x + 1);
