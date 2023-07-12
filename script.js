f596a58f7f27ba88a88139028548fea5df30f676
Kavin Abregana <abreganakavin@gmail.com>
2023-07-12 21:46:18 +0800

    class lecture

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // constructor class
-const Person = function (firstname, birthYear) {
-  // Instance properties
-  this.firstname = firstname;
-  this.birthYear = birthYear;
+// const Person = function (firstname, birthYear) {
+//   // Instance properties
+//   this.firstname = firstname;
+//   this.birthYear = birthYear;
 
-  // Never to this
-  // this.calcAge = function() {
-  //     console.log(2037 - this.birthYear);
-  // }
-};
+//   // Never to this
+//   // this.calcAge = function() {
+//   //     console.log(2037 - this.birthYear);
+//   // }
+// };
 // const jonas = new Person('Jonas', 1992);
 // console.log(jonas);
 // // result: Object { firstname: "Jonas", birthYear: 1992 }
@@ -68,7 +68,6 @@ const Person = function (firstname, birthYear) {
 // console.log(h1);
 // console.dir(x => x + 1);
 
-
 // // coding challenge#1
 // // my solution
 // const Car = function(make, speed){
@@ -108,47 +107,127 @@ const Person = function (firstname, birthYear) {
 // const PersonCL = class{}
 
 // class declaration
-class PersonCl{
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+// class PersonCl{
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
 
-  // Methods will be added to .prototype property
-  calcAge(){
-    console.log(2023 - this.birthYear);
-  }
+//   // Methods will be added to .prototype property
+//   calcAge(){
+//     console.log(2023 - this.birthYear);
+//   }
 
-  greet(){
-    console.log(`Hey ${this.fullName}`)
-  }
+//   greet(){
+//     console.log(`Hey ${this.fullName}`)
+//   }
 
-  get age() {
-    return 2023 - this.birthYear;
-  }
+//   get age() {
+//     return 2023 - this.birthYear;
+//   }
 
-  set fullName(name) {
-    if(name.includes(' ')) this._fullName = name;
-    else alert(`${name} is not a full name!`);
-  }
+//   set fullName(name) {
+//     if(name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
 
-  get fullName() {
-    return this._fullName;  
-  }
-}
-
-const jessica = new PersonCl('Jessica Stellar', 1996)
-console.log(jessica)
-// result: Object { firstname: "Jessica", birthYear: 1996 }
-
-jessica.calcAge();
-// result: 27
-
-console.log(jessica.__proto__ === PersonCl.__proto__);
-// result: true
-
-// PersonCl.prototype.greet = function(){
-//   console.log(`Hey ${this.firstname}`)
+//   get fullName() {
+//     return this._fullName;
+//   }
 // }
-jessica.greet();
-// result: Hey Jessica
+
+// const jessica = new PersonCl('Jessica Stellar', 1996)
+// console.log(jessica)
+// // result: Object { firstname: "Jessica", birthYear: 1996 }
+
+// jessica.calcAge();
+// // result: 27
+
+// console.log(jessica.__proto__ === PersonCl.__proto__);
+// // result: true
+
+// // PersonCl.prototype.greet = function(){
+// //   console.log(`Hey ${this.firstname}`)
+// // }
+// jessica.greet();
+// // result: Hey Jessica
+
+// Object.create
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2023 - this.birthYear);
+//   },
+
+//   init(firstname, birthYear) {
+//     this.firstname = firstname;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+// console.log(steven);
+
+// // creating properties
+// steven.name = 'Steven';
+// steven.birthYear = 2002;
+// steven.calcAge();
+// // result: 21
+
+// // adding new properties better way
+// const sarah = Object.create(PersonProto);
+// sarah.init('Sarah', 1950);
+// sarah.calcAge();
+// // result: 73
+
+// Your tasks:
+// 1. Re-create Challenge #1, but this time using an ES6 class (call it 'CarCl')
+// 2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6)
+// 3. Add a setter called 'speedUS' which sets the current speed in mi/h (but
+// converts it to km/h before storing the value, by multiplying the input by 1.6)
+// 4. Create a new car and experiment with the 'accelerate' and 'brake'
+// methods, and with the getter and setter.
+
+// Test data:
+// § Data car 1: 'Ford' going at 120 km/h
+
+// class CarCl {
+//   constructor(make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+//   }
+
+//   accelerate() {
+//     console.log((this.speed += 10));
+//   }
+
+//   brake() {
+//     console.log((this.speed -= 5));
+//   }
+
+//   get speedUS() {
+//     return this.speed / 1.6;
+//   }
+
+//   set speedUS(speed) {
+//     this.speed = speed * 1.6;
+//   }
+// }
+
+// const ford = new CarCl('Ford', 120);
+// console.log(ford);
+// // result: Object { make: "Ford", speed: 120 }
+// ford.accelerate();
+// // result: 140
+// console.log(ford.speedUS);
+// // result: 81.25
+// ford.accelerate();
+// // result: 140
+
+const Person = function (firstname, birthYear) {
+  this.firstname = firstname;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
